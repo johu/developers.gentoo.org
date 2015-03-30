86de21bbfeb0de246cb1fa7ec2259d176f13b7f1
Johannes Huber <johu@gentoo.org>
2015-03-30 22:52:38 +0200

    Maven spring boot setup

---
+++ src/main/java/org/gentoo/developers/Application.java
@@ -0,0 +1,14 @@
+package org.gentoo.developers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by johu on 30.03.15.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
